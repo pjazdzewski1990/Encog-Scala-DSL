3a8bad32836327bf5505e21cfac88b05562e2f21
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-04-26 16:24:24 +0200

    init

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name:="Encog-Scala-DSL"
+
+scalaVersion :="2.11.6"
+
+version :="0.1"
+
+libraryDependencies += "org.encog" % "encog-core" % "3.3.0"
---
+++ src/main/scala/io/scalac/encog/HelloWorld.scala
@@ -0,0 +1,65 @@
+package io.scalac.encog
+
+import org.encog.Encog;
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+
+import scala.collection.JavaConversions._
+
+object HelloWorld {
+
+  val XOR_INPUT = Array(
+    Array( 0.0, 0.0 ),
+    Array( 1.0, 0.0 ),
+    Array( 0.0, 1.0 ),
+    Array( 1.0, 1.0 )
+  )
+
+  val XOR_IDEAL = Array(
+    Array(0.0),
+    Array(1.0),
+    Array(1.0),
+    Array(0.0)
+  )
+
+  def main(args: Array[String]) {
+    // create a neural network, without using a factory
+    val network = new BasicNetwork();
+    network.addLayer(new BasicLayer(null,true,2));
+    network.addLayer(new BasicLayer(new ActivationSigmoid(),true,3));
+    network.addLayer(new BasicLayer(new ActivationSigmoid(),false,1));
+    network.getStructure().finalizeStructure();
+    network.reset();
+
+    // create training data
+    val trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
+
+    // train the neural network
+    val train = new ResilientPropagation(network, trainingSet);
+
+    var epoch = 1;
+
+    do {
+      train.iteration();
+      println("Epoch #" + epoch + " Error:" + train.getError());
+      epoch = epoch+1;
+    } while(train.getError() > 0.01);
+    train.finishTraining();
+
+    // test the neural network
+    System.out.println("Neural Network Results:");
+    for { pair <- trainingSet.getData() } {
+      val output = network.compute(pair.getInput());
+      println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+        + ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
+    }
+
+    Encog.getInstance().shutdown();
+  }
+}
