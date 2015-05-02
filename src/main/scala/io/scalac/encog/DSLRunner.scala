25ef58e072c23010a48dd17186c7e19e80b8d608
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-05-02 15:04:34 +0200

    matrix operations

--- src/main/scala/io/scalac/encog/DSLRunner.scala
+++ src/main/scala/io/scalac/encog/DSLRunner.scala
@@ -9,6 +9,12 @@ import org.encog.neural.networks.training.propagation.resilient.ResilientPropaga
 object DSLRunner {
   def run(input: Array[Array[Double]], ideal: Array[Array[Double]]) = {
 
+//    1.0 | 1.0 | 1.0 | 0.0 \\
+//    2.0 | 1.0 | 1.0 | 1.0
+//    \\\
+//    1.0 | 1.0 | 1.0 | 0.0 \\
+//    2.0 | 1.0 | 1.0 | 1.0
+
     val network =
       (InputLayer having bias having 2.layers) +
       (ActivationSigmoid having bias having 3.layers) +
--- src/test/scala/io/scalac/encog/dsl/PrimitiveValuesSpec.scala
+++ src/test/scala/io/scalac/encog/dsl/PrimitiveValuesSpec.scala
@@ -19,4 +19,21 @@ class PrimitiveValuesSpec extends FlatSpec with Matchers {
       -1 layers
     }
   }
+
+  "Double" should "allow creating flat Arrays" in {
+    val twoElements = 1.0 | 2.0
+    twoElements shouldBe a [EncogArray2[_]]
+
+    val fourElements = 1.0 | 2.0 | 3.0 | 4.0
+    fourElements shouldBe a [EncogArray4[_]]
+  }
+
+  it should "allows creating matrixes of well defined sizes" in {
+    val matrix =
+      (1.0 | 2.0) \\
+      (3.0 | 4.0)
+    matrix shouldBe a [EncogArray2[_]]
+    matrix.asInstanceOf[EncogArray2[Double]].elem1 shouldBe a [EncogArray2[_]]
+    matrix.asInstanceOf[EncogArray2[Double]].elem2 shouldBe a [EncogArray2[_]]
+  }
 }
